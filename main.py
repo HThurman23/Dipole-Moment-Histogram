a4ab58e9f3e74416509520c5e538ba1079247f92
HThurman23 <hathurman@shockers.wichita.edu>
2024-08-05 14:20:37 -0500

    navigation changes

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pages.page1 as page1
+import pages.page2 as page2
+import pages.page3 as page3
 
 st.set_page_config(page_title="Modular Dashboard", layout="wide")
 
@@ -8,9 +11,8 @@ page = st.sidebar.selectbox("Go to", ["LODIMS Dipole Moment Calculator", "Page 2
 
 # Load the selected page
 if page == "LODIMS Dipole Moment Calculator":
-    import pages.page1 as page1
     page1.app()
 elif page == "Page 2":
-    st.write("Page 2 content here")
+    page2.app()
 elif page == "Page 3":
-    st.write("Page 3 content here")
+    page3.app()
